5a7c98aefac3d2228b19d514053d07a553163bac
oha <x22z@naver.com>
2024-11-21 10:59:32 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# news-server
+## 1. 환경 설정
+
+>[!note] 주의
+> 파이썬 패키지 관리시스템 `anaconda`가 설치되어 있어야 합니다.
+
+``` bash
+git clone {현재 깃 주소}
+cd news-server
+conda create -n zipchack python=3.9 -y
+conda activate zipchack
+pip install -r requirements.txt
+uvicorn main:app --reload
+```
+
+## 2. 테스트
+
+`http://127.0.0.1:8000/docs` 로 swagger 에 접근할 수 있습니다.
+swagger 에서 getNews 메서드를 test 할 수 있습니다.
+
+
---
+++ main.py
@@ -0,0 +1,91 @@
+# main.py
+import requests
+from fastapi import FastAPI
+from bs4 import BeautifulSoup
+from pydantic import BaseModel, Field
+
+
+
+# FastAPI 애플리케이션 인스턴스 생성
+app = FastAPI(
+    title="Zip-chack",  # 문서 제목 수정
+    description="# Zip-chack service APIs \n\n"
+        		+ "## 마음에 드는 집에 착! 집착 서비스 api sample 입니다. \n\n"
+        		+ "### 제공 API : \n\n"
+        		+ "1. __News__ 뉴스기사 크롤링 서비스 \n\n",
+        		
+    version="1.0.0"  # API 버전 수정
+)
+
+
+# 응답 모델 정의
+class NewsItem(BaseModel):
+    dong_code: str = Field(..., description="행정동 코드")
+    title: str = Field(..., description="뉴스 제목")
+    url: str = Field(..., description="뉴스 기사 URL")
+    city_name: str = Field(..., description="도시/구 이름")
+    ref: str = Field(..., description="출처")
+    img: str = Field(..., description="관련사진")
+
+class NewsResponse(BaseModel):
+    news: list[NewsItem]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "news": [
+                    {
+                        "dong_code": "4159000000",
+                        "title": "LH, 동탄2 ‘종합병원 건립 패키지형 개발사업’ 사업자공모 추진",
+                        "url": "https://n.news.naver.com/article/119/0002894004",
+                        "city_name": "화성시",
+                        "ref": "데일리안",
+                        "img":"https://imgnews.pstatic.net/image/009/2024/11/18/0005398589_001_20241118170815139.jpg?type=w860"
+                    },
+                    {
+                        "dong_code": "1165000000",
+                        "title": "서초구 \"미청산 재건축조합 이렇게 관리하세요\"",
+                        "url": "https://n.news.naver.com/article/014/0005269235",
+                        "city_name": "서초구",
+                        "ref": "파이낸셜뉴스",
+                        "img":"https://imgnews.pstatic.net/image/009/2024/11/18/0005398589_001_20241118170815139.jpg?type=w860"
+                    },
+                ]
+            }
+        }
+
+# 기본 경로에 대한 get 요청 처리
+@app.get("/news", tags=["News"], response_model=NewsResponse)
+def getNewsList():
+    search = "https://land.naver.com/news/region.naver?city_no=1100000000&dvsn_no="
+    response = requests.get(search)
+    soup = BeautifulSoup(response.text, "html.parser")
+    dong_codes = soup.select(".area") # 결과는 리스트
+
+    links = soup.select(".category_list li a:nth-of-type(2)") 
+    response = {"news":[]}
+
+    idx = 0
+    for link, code in zip(links, dong_codes):
+        city_name = code.text[1:-1]
+        dong_code = code.attrs['href'].split("dvsn_no")[1][1:]
+        url = link.attrs['href']
+
+        title_soup = BeautifulSoup(requests.get(url).text, "html.parser")
+        title = title_soup.select('#title_area span')[0].text
+        ref = title_soup.select('.ofhd_float_title_text_press')[0].text
+        img = title_soup.select('#img1')[0].attrs['data-src']
+
+        data = {"dong_code":dong_code, "title":title,"url":url,"city_name":city_name, "ref":ref, "img":img}
+        response["news"].append(data)
+        if idx==3: break
+        idx+=1
+
+
+    return response
+
+
+# # POST 요청 예시: 데이터를 받아서 응답하는 엔드포인트
+# @app.post("/items/")
+# def create_item(item: dict):
+#     return {"message": "Item created", "data": item}
